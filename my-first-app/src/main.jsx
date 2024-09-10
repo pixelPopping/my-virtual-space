aa9445dcbaaa6a5cda3ab0115b513c0574c03be1
Yorian Fransz <syndey-cook@outlook.com>
2024-09-10 18:28:08 +0200

    add

--- my-first-app/src/main.jsx
+++ my-first-app/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App."; // Correct import statement
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import * as React from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+      <TheRestOfYourApplication />
+    </ChakraProvider>
+  );
+}
