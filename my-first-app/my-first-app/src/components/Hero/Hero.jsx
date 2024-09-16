ce59be983f2c830d09128bea4b87718163c1827d
Yorian Fransz <syndey-cook@outlook.com>
2024-09-16 22:06:54 +0200

    add some changes

--- my-first-app/my-first-app/src/components/Hero/Hero.jsx
+++ my-first-app/my-first-app/src/components/Hero/Hero.jsx
@@ -12,9 +12,10 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi Welcome, I’m Yorian</h1>
         <p className={styles.description}>
-          I am a Frontend developer student, in my previous career i worked as a
-          chef and showcased my creativity in cooking. Now I study web
-          development, and I’m eager to learn the world of IT.
+          I am a Front-end developer student from the Netherlands. In my
+          previous career i worked as a chef and i showcased my creativity in
+          cooking. Now I study web development, and I’m eager to learn the world
+          of IT.
         </p>
         <a href="mailto:sydney-cook@outlook.com" className={styles.contactBtn}>
           Contact Me
