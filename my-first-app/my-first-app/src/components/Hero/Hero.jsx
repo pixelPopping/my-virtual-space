9124558a156a0ba84e645de24a69eea9f22ab0b4
Yorian Fransz <syndey-cook@outlook.com>
2024-09-16 17:04:54 +0200

    add some code

---
+++ my-first-app/my-first-app/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+  return (
+    <section>
+      <div>
+        <h1>Hi Welcome, I,m Yorian</h1>
+        <p>
+          Iam a Frontend developer student, and this is my Portfolio. In my
+          previous carreer i work as a chef and show my creativity in cooking.
+          now im studying web development and im eager to learn the world of IT,
+          this is the showcasse of my work
+        </p>
+      </div>
+    </section>
+  );
+};
