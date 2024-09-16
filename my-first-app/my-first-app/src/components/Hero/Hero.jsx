7ddcfab3f246c9a28c0d035edde2481c1cfa5bab
Yorian Fransz <syndey-cook@outlook.com>
2024-09-16 18:39:43 +0200

    change code

--- my-first-app/my-first-app/src/components/Hero/Hero.jsx
+++ my-first-app/my-first-app/src/components/Hero/Hero.jsx
@@ -1,17 +1,25 @@
+import { getImageUrl } from "../../utils";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1>Hi Welcome, I’m Yorian</h1>
-        <p>
+        <h1 className={styles.title}>Hi Welcome, I’m Yorian</h1>
+        <p className={styles.description}>
           I am a Frontend developer student, and this is my portfolio. In my
           previous career, I worked as a chef and showcased my creativity in
           cooking. Now I’m studying web development, and I’m eager to learn the
           world of IT. This is the showcase of my work.
         </p>
-        <a href="mailto:sydney-cook@outlook.com">Contact Me</a>
+        <a href="mailto:sydney-cook@outlook.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+        <img
+          src={getImageUrl("assets/Hero/Hero.png")}
+          alt="hero"
+          className={styles.heroImage}
+        />
         <div className={styles.topBlur} />
         <div className={styles.bottomBlur} />
       </div>
