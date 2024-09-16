f9b12961be6fbb3d481b7cea5a627bc0fe7d9862
Yorian Fransz <syndey-cook@outlook.com>
2024-09-16 22:25:12 +0200

    add mod delete

--- my-first-app/my-first-app/src/components/Hero/Hero.jsx
+++ my-first-app/my-first-app/src/components/Hero/Hero.jsx
@@ -3,9 +3,9 @@ import styles from "./Hero.module.css";
 
 export const Hero = () => {
   const imageUrl = getImageUrl(
-    "24325541_2022_ani_cartoon_27-removebg-preview-1.png"
+    "assets/nav/hero/24325541_2022_ani_cartoon_27-removebg-preview 1.png"
   );
-  console.log(imageUrl); // This should print: /hero/24325541_2022_ani_cartoon_27-removebg-preview-1.png
+  console.log(imageUrl); // Check this URL in the browser or console
 
   return (
     <section className={styles.container}>
@@ -13,21 +13,16 @@ export const Hero = () => {
         <h1 className={styles.title}>Hi Welcome, I’m Yorian</h1>
         <p className={styles.description}>
           I am a Front-end developer student from the Netherlands. In my
-          previous career, I worked as a chef and showcased my creativity in
+          previous career i worked as a chef and i showcased my creativity in
           cooking. Now I study web development, and I’m eager to learn the world
           of IT.
         </p>
         <a href="mailto:sydney-cook@outlook.com" className={styles.contactBtn}>
           Contact Me
         </a>
-        <img
-          src={imageUrl}
-          alt="Cartoon of Yorian the developer"
-          className={styles.heroImage}
-          loading="lazy"
-        />
-        <div className={styles.topBlur} aria-hidden="true" />
-        <div className={styles.bottomBlur} aria-hidden="true" />
+        <img src={getImageUrl} alt="hero" className={styles.heroImage} />
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
       </div>
     </section>
   );
